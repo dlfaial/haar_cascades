63bc4e7efceee7dd2a9d26443f4845de1acaeb44
DLFAIAL <dfaial@id.uff.br>
2016-02-06 03:14:00 -0600

    haarcascades

---
+++ opencv_face_detection.py
@@ -0,0 +1,34 @@
+import sys
+import cv2
+
+
+
+# Here we get the values supplied by the user
+
+imagePath = "/Users/david/Dropbox/lewis/second_semester/ai/abba.png"
+cascPath = "/Users/david/Dropbox/lewis/second_semester/ai/haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+)
+
+print "Found {0} faces!".format(len(faces))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", gray)
+cv2.waitKey(0)
